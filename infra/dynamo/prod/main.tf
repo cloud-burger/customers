5e23ff44f4c37d23806a564675083a4c9afc92e1
Gabrielmadrid73 <50714922+Gabrielmadrid73@users.noreply.github.com>
2025-01-30 22:54:21 -0300

    feature/infra - Added terraform RDS (#2)
    * feature/infra - Added terraform RDS
    
    * feature/infra - Added DynamoDB and removed unecessary resources

---
+++ infra/dynamo/prod/main.tf
@@ -0,0 +1,27 @@
+resource "aws_dynamodb_table" "customers" {
+  name           = "${var.project}-${var.environment}"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  attribute {
+    name = "document_number"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name               = "document_number_gsi"
+    hash_key           = "document_number"
+    projection_type    = "INCLUDE"
+    non_key_attributes = ["id"]
+  }
+
+  tags = {
+    Name        = "dynamodb-table-1"
+    Environment = "production"
+  }
+}
